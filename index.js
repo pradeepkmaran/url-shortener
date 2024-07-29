bcabf9967de07983f7e1f1fd832457f3f49495bc
pradeepkmaran <pradeepkm6122004@gmail.com>
2024-07-29 21:54:55 +0530

    SSR using EJS

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
+const path = require('path');
 const { router: urlRoute } = require('./routes/url');
 const { connectMongoDB } = require('./connection');
 const { URL } = require('./models/url');
 const { handleGetAnalytics } = require('./controllers/url');
+const { router: staticRoute } = require('./routes/staticrouter'); 
+const { ejs } = require('ejs');
 
 const app = express();
 const PORT = 8001;
 
-
 connectMongoDB('mongodb://127.0.0.1:27017/url-shortener')
 .then(() => console.log('Connected to DB'));
 
-app.use(express.json());
-app.use('/url', urlRoute);
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
-app.get('/:shortId', async (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/url', urlRoute);
+app.use('/', staticRoute);
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
